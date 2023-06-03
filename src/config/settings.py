0f5015ea1359dcd10ef6df324b6c79728a16e4a6
rakibulislam8226 <rakibulislam8226@gmail.com>
2023-06-03 13:06:28 +0600

    [FIX] dir fix

--- src/config/settings.py
+++ src/config/settings.py
@@ -140,6 +140,16 @@ STATIC_URL = "static/"
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
+# alert message here
+from django.contrib.messages import constants as messages
+
+MESSAGE_TAGS = {
+    messages.DEBUG: 'alert-info',
+    messages.INFO: 'alert-info',
+    messages.SUCCESS: 'alert-success',
+    messages.WARNING: 'alert-warning',
+    messages.ERROR: 'alert-danger',
+}
 
 
 APP_SITE_HEADER = "Jana"
